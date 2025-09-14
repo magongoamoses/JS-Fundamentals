ef766e88c3892f7d895328b4f0514c6e8ff619ef
Magongoa Moses <magongoamoses@gmail.com>
2025-09-14 18:00:00 +0200

    updated the file

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,7 +1,7 @@
 const languages = ["C is fun","Python is cool","JavaScript is amazing"];
 
 let i = 0;
-while(i < languages.length){
+while (i < languages.length){
     console.log(languages[i]);
     i++;
 }
