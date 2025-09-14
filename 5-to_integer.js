3303b23c23dff0f80d98ca411658d1c920ef1484
Magongoa Moses <magongoamoses@gmail.com>
2025-09-14 17:11:15 +0200

    update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -2,7 +2,7 @@ const arguMents = process.argv.slice(2);
 const firstArg = parseInt(arguMents[0]);
 
 if(isNaN(firstArg)){
-    console.log("Not a number ");
+    console.log("Not a number");
 }
 else{
     console.log("My number:", firstArg);
